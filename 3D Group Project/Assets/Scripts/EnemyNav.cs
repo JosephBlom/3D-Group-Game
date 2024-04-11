23538f48d35096350ef90b0c9113dcc3e99df4aa
JosephBlom <24.joseph.blom@ksd.org>
2024-04-11 12:02:08 -0700

    Began Work on AI

---
+++ 3D Group Project/Assets/Scripts/EnemyNav.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNav : MonoBehaviour
+{
+    NavMeshAgent agent;
+    [SerializeField] GameObject player;
+    [SerializeField] float chaseDistance;
+    Vector3 home;
+
+    private void Start()
+    {
+        home = transform.position;
+        agent = GetComponent<NavMeshAgent>();
+    }
+    private void Update()
+    {
+        Vector3 moveDirection = player.transform.position - transform.position;
+        if (moveDirection.magnitude < chaseDistance)
+        {
+            agent.destination = player.transform.position;
+        }
+        else
+        {
+            agent.destination = home;
+        }
+    }
+}
