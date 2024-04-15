c64ea966125b7832ed9e0641a6d0c2cfedd7225a
JosephBlom <24.joseph.blom@ksd.org>
2024-04-15 12:41:03 -0700

    Created a basic inventory system

--- 3D Group Project/Assets/Scripts/Inventory/Item.cs
+++ 3D Group Project/Assets/Scripts/Inventory/Item.cs
@@ -4,5 +4,9 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    
+    public new string name = "New Item";
+    public string description = "New Description";
+    public Sprite icon;
+    public int currentQuantity = 1;
+    public int maxQuantity = 16;
 }
--- 3D Group Project/Assets/Scripts/Inventory/Slot.cs
+++ 3D Group Project/Assets/Scripts/Inventory/Slot.cs
@@ -1,8 +1,71 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
 
-public class Slot : MonoBehaviour
+public class Slot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    
+    public bool hovered;
+    public Item heldItem;
+
+    private Color opaque = new Color(1, 1, 1, 1);
+    private Color transparent = new Color(1, 1, 1, 0);
+
+    private Image thisSlotImage;
+
+    public TMP_Text thisSlotQuantityText;
+
+    public void initialiseSlot()
+    {
+        thisSlotImage = gameObject.GetComponent<Image>();
+        thisSlotQuantityText = transform.GetChild(0).GetComponent<TMP_Text>();
+        thisSlotImage.sprite = null;
+        thisSlotImage.color = transparent;
+        setItem(null);
+    }
+
+    public void setItem(Item item)
+    {
+        heldItem = item;
+
+        if(item != null)
+        {
+            thisSlotImage.sprite = heldItem.icon;
+            thisSlotImage.color = opaque;
+            updateData();
+        }
+        else
+        {
+            thisSlotImage.sprite = null;
+            thisSlotImage.color = transparent;
+            updateData();
+        }
+    }
+
+    public Item getItem()
+    {
+        return heldItem;
+    }
+
+    public void updateData()
+    {
+        if(heldItem != null)
+        {
+            thisSlotQuantityText.text = heldItem.currentQuantity.ToString();
+        }
+        else
+        {
+            thisSlotQuantityText.text = "";
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        hovered = true;
+    }
+
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        hovered = false;
+    }
 }
