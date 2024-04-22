2cb332d56691b84668865da4d049407ef82dde5a
JosephBlom <24.joseph.blom@ksd.org>
2024-04-22 13:24:05 -0700

    Finished a settings menu

--- 3D Group Project/Assets/Scripts/Pause and Settings/Settings.cs
+++ 3D Group Project/Assets/Scripts/Pause and Settings/Settings.cs
@@ -2,9 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Settings : MonoBehaviour
 {
     public float lookSensitivity;
+    public TextMeshProUGUI valueText;
     public bool timer;
+
+    GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("PlayerObject");
+        valueText.text = player.GetComponent<StarterAssets.FirstPersonController>().RotationSpeed.ToString();
+    }
+
+    public void SetSensitivity(float sens)
+    {
+        lookSensitivity = sens;
+        valueText.text = sens.ToString();
+        player.GetComponent<StarterAssets.FirstPersonController>().RotationSpeed = lookSensitivity;
+    }
+
+    public void TimerOnOff()
+    {
+
+    }
 }
