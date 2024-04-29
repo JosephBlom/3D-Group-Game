226007f413ade1e815a5dd4603dee03181487b5e
JosephBlom <24.joseph.blom@ksd.org>
2024-04-29 12:44:30 -0700

    Created basic travel music

---
+++ 3D Group Project/Assets/Scripts/Music & Sound/MusicManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public List<AudioClip> soundtracks = new List<AudioClip>();
+    public int currentClip = 0;
+    public Slider volumeSlider;
+
+    private void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            playNextSong();
+        }
+    }
+
+    void playNextSong()
+    {
+        currentClip++;
+        if (currentClip > soundtracks.Count -1)
+        {
+            currentClip = 0;
+        }
+        audioSource.clip = soundtracks[currentClip];
+        audioSource.Play();
+    }
+
+    public void SetVolume()
+    {
+        audioSource.volume = volumeSlider.value;
+    }
+}
