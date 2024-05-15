d72a03778f98d4326026f2bdc672d9fe1f880cb4
AR-Reynolds <147757931+AR-Reynolds@users.noreply.github.com>
2024-05-15 12:41:32 -0700

    changes and new gun system

---
+++ 3D Group Project/Assets/Scripts/PlayerWeaponHotkey.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeaponHotkey : MonoBehaviour
+{
+    [SerializeField] private GameObject automatic;
+    [SerializeField] private GameObject shotgun;
+    [SerializeField] private GameObject rocketLauncher;
+
+    private GameObject equipped;
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            UnequipWeapon(shotgun);
+            UnequipWeapon(rocketLauncher);
+
+            EquipWeapon(automatic);
+            equipped = automatic;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            UnequipWeapon(automatic);
+            UnequipWeapon(rocketLauncher);
+
+            EquipWeapon(shotgun);
+            equipped = shotgun;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            UnequipWeapon(shotgun);
+            UnequipWeapon(automatic);
+
+            EquipWeapon(rocketLauncher);
+            equipped = rocketLauncher;
+        }
+    }
+
+    private void UnequipWeapon(GameObject equipped)
+    {
+        equipped.SetActive(false);
+    }
+    private void EquipWeapon(GameObject weapontoEquip)
+    {
+        weapontoEquip.SetActive(true);
+    }
+
+
+}
